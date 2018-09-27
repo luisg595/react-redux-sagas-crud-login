f6b1638f628efed40f2cf83e5fe0f475b38e05ea
luisg595 <luisg595@gmail.com>
2018-09-27 11:57:06 -0300

    redirect with a middleware to /

--- src/middlewares/AuthRedirect.js
+++ src/middlewares/AuthRedirect.js
@@ -3,7 +3,7 @@ import history from '../history'
 const AuthRedirect = store => next => action => {
     if (action.type === 'LOGIN_SUCCESS') {
         sessionStorage.setItem('login', JSON.stringify(action.payload))
-        history.push('/users')
+        history.push('/')
     }
     next(action)
 }
